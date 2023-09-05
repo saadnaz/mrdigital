d3a22647cd543cb98c7064e9ddaa40874e4d5daa
saadnaz <ouldzein.saadna@gmail.com>
2023-09-05 18:47:32 +0000

    add tr digi, responsive img 2

--- src/components/CTA2.jsx
+++ src/components/CTA2.jsx
@@ -3,12 +3,12 @@ import Button from "./Button";
 import { bgVidNum } from "../assets"
 
 const CTA2 = () => (
-  <section className={`${styles.flexCenter} sm:flex-row flex-col bg-black-gradient-2 rounded-[20px] box-shadow`}>
-    <div className="divVideo flex-1 flex flex-col">
-      <video autoPlay loop muted id='video'>
+  <section className={`${styles.flexCenter} sm:flex-row`}>
+    <div className="divVideo">
+      <video autoPlay loop muted id='video' >
         <source src={bgVidNum} type="video/mp4" />
       </video>
-      <p className={`upVideo ${styles.paragraph} text-xl md:text-2xl lg:text-3xl xl:text-5xl`}>
+      <p className={`upVideo ${styles.paragraph} text-xl md:text-2xl lg:text-3xl xl:text-5xl `}>
         mrDigital, votre cabinet de conseil
         en Cybersécurité et Stratégie Digitale
 
