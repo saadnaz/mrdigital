fc3ff4c6c896ed590a0942664242e1520fa5f0fe
saadnaz <ouldzein.saadna@gmail.com>
2023-09-05 18:17:45 +0000

    add tr digi, responsive img

--- src/components/CTA2.jsx
+++ src/components/CTA2.jsx
@@ -3,7 +3,7 @@ import Button from "./Button";
 import { bgVidNum } from "../assets"
 
 const CTA2 = () => (
-  <section className={`${styles.flexCenter} ${styles.marginY}  sm:flex-row flex-col bg-black-gradient-2 rounded-[20px] box-shadow`}>
+  <section className={`${styles.flexCenter} sm:flex-row flex-col bg-black-gradient-2 rounded-[20px] box-shadow`}>
     <div className="divVideo flex-1 flex flex-col">
       <video autoPlay loop muted id='video'>
         <source src={bgVidNum} type="video/mp4" />
--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -42,7 +42,7 @@ const Hero = () => {
       </div>
 
       <div className={`flex-1 flex ${styles.flexCenter} md:my-0 my-10 relative`}>
-        <img src={bgImgNum} alt="billing" rounded className="rounded-[20px] w-[90%] h-[90%] xl:w-[100%] xl:h-[100%] relative z-[5]" />
+        <img src={bgImgNum} rounded className="rounded-[20px] sm:w-5/6  lg:w-4/5 xl:w-full xl:h-full relative z-[5]" />
 
         {/* gradient start */}
         <div className="absolute z-[0] w-[40%] h-[35%] top-0 pink__gradient" />
