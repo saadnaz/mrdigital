47534e9a006ede3c6810ccadc953802f4da5ef10
saadnaz <ouldzein.saadna@gmail.com>
2023-09-12 18:45:09 +0000

    responsive button

--- src/components/GetStarted.jsx
+++ src/components/GetStarted.jsx
@@ -2,7 +2,7 @@ import styles from "../style";
 import { arrowUp } from "../assets";
 
 const GetStarted = () => (
-  <div className={`${styles.flexCenter} w-[140px] h-[140px] rounded-full bg-blue-gradient p-[2px] cursor-pointer`}>
+  <div className={`${styles.flexCenter} w-[150px] h-[150px] md:ml-16 rounded-full bg-blue-gradient p-[2px] cursor-pointer`}>
     <a className={`${styles.flexCenter} flex-col bg-primary w-[100%] h-[100%] rounded-full`} href="#contact">
       <div className={`${styles.flexStart} flex-row`}>
         <p className="font-poppins font-medium text-[18px] leading-[23.4px]">
